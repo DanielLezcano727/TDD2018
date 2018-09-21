fb29bf4bbe4f17b392a60ee66e67dc7f9dffed66
Daniel Lezcano <daniel.lezcano727@gmail.com>
2018-09-21 00:14:20 -0300

    Añadido test que verifica el palo de una carta

---
+++ tests/CartaTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TDD;
+
+use PHPUnit\Framework\TestCase;
+
+class MazoTest extends TestCase {
+
+    public function testPalo(){
+        $carta = new Carta(12,"Copa");
+        $this->assertEquals($carta->verPalo(),"Copa");
+        $carta = new Carta(12,"Basto");
+        $this->assertEquals($carta->verPalo(),"Basto");
+        $carta = new Carta(12,"Espada");
+        $this->assertEquals($carta->verPalo(),"Espada");
+        $carta = new Carta("As", "Picas");
+        $this->assertEquals($carta->verPalo(),"Picas");
+        $carta = new Carta("As", "Hola");        
+        $this->assertNull($carta);
+    }
+
+}
