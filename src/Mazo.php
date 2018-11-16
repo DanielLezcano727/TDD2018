bf0a0d7979d62f83322edd7c8412d6c55a137e73
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-11-16 14:23:50 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Mazo.php
+++ src/Mazo.php
@@ -13,45 +13,45 @@ class Mazo {
         if($tipo == "españolas"){
             $this->poker = false;
         }else{
-        	$this->poker = true;
+          $this->poker = true;
         }
     }
 
-	/**
-	 * mezclar: Mezcla el array cartas perteneciente a la clase
-	 * 
-	 * @return bool
-	 * 
-	 */
+  /**
+   * mezclar: Mezcla el array cartas perteneciente a la clase
+   * 
+   * @return bool
+   * 
+   */
 
     public function mezclar() {
         shuffle($this->cartas);
-        return true;
+        return TRUE;
     }
 
 
-	/**
-	 * tieneCartas: Devuelve true si el array cartas tiene algun elemento dentro.
-	 * 
-	 * @return bool
-	 * 
-	 */
+  /**
+   * tieneCartas: Devuelve true si el array cartas tiene algun elemento dentro.
+   * 
+   * @return bool
+   * 
+   */
 
-    public function tieneCartas(){
+    public function tieneCartas() {
         return $this->cant != 0;
     }
 
-	/**
-	 * agregar: Agrega una carta al array cartas perteneciente a la clase
-	 * 
-	 * @param int $numero
-	 * @param string $numero
-	 * @param string $palo
-	 * 
-	 * @return bool
-	 */
+  /**
+   * agregar: Agrega una carta al array cartas perteneciente a la clase
+   * 
+   * @param int $numero
+   * @param string $numero
+   * @param string $palo
+   * 
+   * @return bool
+   */
 		
-	public function agregar($numero, $palo){
+  public function agregar($numero, $palo){
         if($this->poker){
             if(!(($numero > 1 && $numero <= 10) || $numero == "As" || $numero == "K" || $numero == "Q" || $numero == "J")){
                 $numero = "As";
@@ -63,41 +63,41 @@ class Mazo {
         $carta = new Carta($numero, $palo);
         array_unshift($this->cartas, $carta);
         $this->cant++;
-        return true;
+        return TRUE;
     }
 
-	/**
-	 * obtenerCantidad: Devuelve la cantidad de cartas que tiene el mazo
-	 * 
-	 * @return int 
-	 */
+  /**
+   * obtenerCantidad: Devuelve la cantidad de cartas que tiene el mazo
+   * 
+   * @return int 
+   */
 
-    public function obtenerCantidad(){
+    public function obtenerCantidad() {
         return $this->cant;
-	}
+  }
 	
-	/**
-	 * obtenerCarta: Devuelve la primera carta que se agrego al mazo
-	 * 
-	 * @return Carta
-	 */
+  /**
+   * obtenerCarta: Devuelve la primera carta que se agrego al mazo
+   * 
+   * @return Carta
+   */
 
-    public function obtenerCarta(){
+    public function obtenerCarta() {
         return array_pop($this->cartas);
-	}
+  }
 	
-	/**
-	 * cortar: Toma una cantidad determinada de cartas del mazo, las quita y las agrega al inicio del array
-	 * 
-	 * @param int
-	 * 
-	 * @return void
-	 */
+  /**
+   * cortar: Toma una cantidad determinada de cartas del mazo, las quita y las agrega al inicio del array
+   * 
+   * @param int
+   * 
+   * @return void
+   */
 
     public function cortar($cant){
         for($i = 0; $i<$cant;$i++){
-			$aux = $this->obtenerCarta();
-			$this->agregar($aux->verNumero(),$aux->verPalo());
+      $aux = $this->obtenerCarta();
+      $this->agregar($aux->verNumero(),$aux->verPalo());
         }
     }
 }
