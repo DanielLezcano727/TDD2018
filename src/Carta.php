8c6f7613f71559893bd301aaf04b6c7f050d7375
Micaela Siles <micaela.b.siles@gmail.com>
2018-11-16 12:40:56 -0300

    Añadido standard de codigo Drupla

--- src/Carta.php
+++ src/Carta.php
@@ -4,79 +4,76 @@ namespace TDD;
 
 class Carta {
 
-    protected $numero;
-    protected $palo;
+  protected $numero;
+  protected $palo;
 
-    /**
-     * Constructor: Contruye una carta 
-     * 
-     * @param int $numero
-     * @param string $numero
-     * @param string $palo
-     */
+  /**
+   * Constructor: Contruye una carta
+   *
+   * @param int $numero
+   * @param string $numero
+   * @param string $palo
+   */
+  public function __construct($numero, $palo) {
 
-    public function __construct($numero, $palo) {
-
-        switch (strtolower($palo)) {
-            case "copa":
-            case "basto":
-            case "espada":
-            case "oro":
-            case "picas":
-            case "corazones":
-            case "diamantes":
-            case "treboles":
-                $this->palo = $palo;
-                break;
-            default:
-                $this->palo = "Copa";
-                break;
-        }
-
-        if ($numero <= 12 && $numero > 0) {
-            $this->numero = $numero;
-        }else {
-            switch (strtolower($numero)) {
-                case "as":
-                case "k":
-                case "q":
-                case "j":
-                    $this->numero = $numero;
-                    break;
-                default:
-                    $this->numero = 2;
-            }
-        }
+    switch (strtolower($palo)) {
+      case "copa":
+      case "basto":
+      case "espada":
+      case "oro":
+      case "picas":
+      case "corazones":
+      case "diamantes":
+      case "treboles":
+        $this->palo = $palo;
+        break;
+      default:
+        $this->palo = "Copa";
+        break;
     }
 
-    /**
-     * verPalo: Devuelve el palo de la carta
-     * 
-     * @return string
-     */
-
-    public function verPalo() {
-        return $this->palo;
+    if ($numero <= 12 && $numero > 0) {
+      $this->numero = $numero;
     }
-
-    /**
-     * verNumero: Devuelve el valor de la carta.
-     * 
-     * @ return int or string
-     */
-
-    public function verNumero() {
-          return $this->numero;
+    else {
+      switch (strtolower($numero)) {
+        case "as":
+        case "k":
+        case "q":
+        case "j":
+          $this->numero = $numero;
+          break;
+        default:
+          $this->numero = 2;
+      }
     }
+  }
 
-    /**
-     * leer: Devuelve la carta como un string
-     * 
-     * @return string
-     */
+  /**
+   * verPalo: Devuelve el palo de la carta
+   *
+   * @return string
+   */
+  public function verPalo() {
+    return $this->palo;
+  }
 
-    public function leer() {
-        return $this->verNumero() . " de " . $this->verPalo();
-    }
+  /**
+   * verNumero: Devuelve el valor de la carta.
+   *
+   * @ return int or string
+   */
+  public function verNumero() {
+    return $this->numero;
+  }
+
+  /**
+   * leer: Devuelve la carta como un string
+   *
+   * @return string
+   */
+  public function leer() {
+    return $this->verNumero() . " de " . $this->verPalo();
+  }
 
 }
--- src/Mazo.php
+++ src/Mazo.php
@@ -3,101 +3,89 @@
 namespace TDD;
 
 class Mazo {
-    protected $cartas;
-    protected $cant;
-    protected $poker;
+  protected $cartas;
+  protected $cant;
+  protected $poker;
 
-    public function __construct($tipo = "españolas"){
-        $this->cartas = array();
-        $this->cant = 0;
-        if($tipo == "españolas"){
-            $this->poker = false;
-        }else{
-          $this->poker = true;
-        }
+  public function __construct($tipo = "españolas") {
+    $this->cartas = array();
+    $this->cant = 0;
+    if ($tipo == "españolas") {
+      $this->poker = false;
     }
+    else {
+      $this->poker = true;
+    }
+  }
 
   /**
    * mezclar: Mezcla el array cartas perteneciente a la clase
-   * 
+   *
    * @return bool
-   * 
+   *
    */
-
-    public function mezclar() {
-        shuffle($this->cartas);
-        return TRUE;
-    }
-
+  public function mezclar() {
+    shuffle($this->cartas);
+    return TRUE;
+  }
 
   /**
    * tieneCartas: Devuelve true si el array cartas tiene algun elemento dentro.
-   * 
+   *
    * @return bool
-   * 
+   *
    */
-
-    public function tieneCartas() {
-        return $this->cant != 0;
-    }
+  public function tieneCartas() {
+    return $this->cant != 0;
+  }
 
   /**
    * agregar: Agrega una carta al array cartas perteneciente a la clase
-   * 
+   *
    * @param int $numero
    * @param string $numero
    * @param string $palo
-   * 
+   *
    * @return bool
    */
-		
-  public function agregar($numero, $palo){
-        if($this->poker){
-            if(!(($numero > 1 && $numero <= 10) || $numero == "As" || $numero == "K" || $numero == "Q" || $numero == "J")){
-                $numero = "As";
-            }
-            if(!(strtolower($palo) == "picas" || strtolower($palo) == "diamantes" || strtolower($palo) == "corazones" || strtolower($palo) == "treboles")){
-                $palo = "Picas";
-            }
-        }
-        $carta = new Carta($numero, $palo);
-        array_unshift($this->cartas, $carta);
-        $this->cant++;
-        return TRUE;
-    }
+  public function agregar($numero, $palo) {
+    $carta = new Carta($numero, $palo);
+    array_unshift($this->cartas, $carta);
+    $this->cant++;
+    return TRUE;
+  }
 
   /**
    * obtenerCantidad: Devuelve la cantidad de cartas que tiene el mazo
-   * 
-   * @return int 
+   *
+   * @return int
    */
-
-    public function obtenerCantidad() {
-        return $this->cant;
+  public function obtenerCantidad() {
+    return $this->cant;
   }
-	
+
   /**
    * obtenerCarta: Devuelve la primera carta que se agrego al mazo
-   * 
+   *
    * @return Carta
    */
 
-    public function obtenerCarta() {
-        return array_pop($this->cartas);
+  public function obtenerCarta() {
+    return array_pop($this->cartas);
   }
-	
+
   /**
    * cortar: Toma una cantidad determinada de cartas del mazo, las quita y las agrega al inicio del array
-   * 
+   *
    * @param int
-   * 
+   *
    * @return void
    */
 
-    public function cortar($cant){
-        for($i = 0; $i<$cant;$i++){
+  public function cortar($cant) {
+    for ($i = 0; $i < $cant; $i++) {
       $aux = $this->obtenerCarta();
-      $this->agregar($aux->verNumero(),$aux->verPalo());
-        }
+      $this->agregar($aux->verNumero(), $aux->verPalo());
     }
+  }
 }
